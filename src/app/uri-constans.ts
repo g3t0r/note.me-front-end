cf77281236f65fb66dc53556f26142256a21f99f
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2019-05-24 21:52:30 +0200

    Changes to dockerize it

--- src/app/uri-constans.ts
+++ src/app/uri-constans.ts
@@ -1,8 +1,9 @@
 export const REGISTRATION_URI_DEV = 'http://localhost:8080/api/users/register';
 export const LOGIN_URI_DEV = 'http://localhost:8080/api/login';
-export const REGISTRATION_URI_PROD =
-  'https://notem3-resources.herokuapp.com/api/users/register';
-export const LOGIN_URI_PROD =
-  'https://notem3-resources.herokuapp.com/api/login';
+// export const REGISTRATION_URI_PROD = 'https://notem3-resources.herokuapp.com/api/users/register';
+export const REGISTRATION_URI_PROD = 'http://localhost:8080/api/users/register';
+// export const LOGIN_URI_PROD = 'https://notem3-resources.herokuapp.com/api/login';
+export const LOGIN_URI_PROD = 'http://localhost:8080/api/login';
 export const BASE_URI_DEV = 'http://localhost:8080';
-export const BASE_URI_PROD = 'https://notem3-resources.herokuapp.com';
+// export const BASE_URI_PROD = 'https://notem3-resources.herokuapp.com';
+export const BASE_URI_PROD = 'http://localhost:8080';
